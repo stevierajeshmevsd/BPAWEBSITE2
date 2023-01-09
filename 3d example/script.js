405e26b36cc51833ddae7e2eff1e02997af22114
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2023-01-09 17:16:46 -0500

    Update 12
    Added Stufff

--- 3d example/script.js
+++ 3d example/script.js
@@ -6,7 +6,7 @@ function CarConditionalQuery(/**/) {
   const limit = args[0];
 
   // Build the query string 
-  let query = String('SELECT cars.ID, carBrand.Brand, carModel.Model, cars.trim, cars.manufacturingYear, cars.MSRP, cars.buyDate FROM carBrand INNER JOIN carModel ON carBrand.ID = carModel.FK_Brand INNER JOIN cars ON carModel.ID = cars.FK_Model' + args.slice(1).join(" ") + ' LIMIT ' + limit)
+  let query = String('SELECT cars.ID, carBrand.Brand, carModel.Model, cars.car, cars.manufacturingYear, cars.MSRP, cars.buyDate, cars.image FROM carBrand INNER JOIN carModel ON carBrand.ID = carModel.FK_Brand INNER JOIN cars ON carModel.ID = cars.FK_Model' + args.slice(1).join(" ") + ' LIMIT ' + limit)
 
   // Log the query to the console
   console.log(query)
@@ -27,10 +27,20 @@ function CarConditionalQuery(/**/) {
       
 
       //intialize local variables
-      let rowdata = ''; // create empty text variable
-      let columnID = Object.keys(data[0]); // create an array with the object names in columnID
+      let keys = Object.keys(data[0]).reverse();
+      let result = data.reverse();
+      let innerText = '<p>';
+      for (let r = result.length; r--; r > 0) {
 
-      console.log(data, columnID)
+
+        for (let c = keys.length; c--; c > 0) {
+          innerText = innerText + keys[c] + '| ' + result[r][ keys[c] ] + '| '
+        }
+
+        innerText = innerText + '</p><br><p>';
+      }
+      console.log(innerText)
+      document.getElementById('results').innerHTML = innerText;
 
       // clear the results div
       document.getElementById("results").innerHTML
---
+++ js/query.js
@@ -0,0 +1,13 @@
+// // document.getElementById('desc').innerHTML = window.variable; 
+// console.log("This is the right place for this message to be!lol")
+
+// // import { myObject } from "./cars";
+// import { myObject } from "./cars";
+// console.log(myObject.value);
+// console.log(myObject.name);
+
+
+// import { varname } from "./cars.js";
+
+
+// console.log(varname);
